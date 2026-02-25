dcfbc3676bf82704b403c61a1855087b0afb0097
cerque1 <127020229+cerque1@users.noreply.github.com>
2026-02-25 12:44:05 +0300

    Fix call media pipeline for Qt 6.9 APIs

--- callsession.h
+++ callsession.h
@@ -3,6 +3,7 @@
 
 #include <QObject>
 #include <QString>
+#include <QByteArray>
 #include <memory>
 
 #ifdef HAVE_LIBDATACHANNEL
@@ -19,22 +20,26 @@ public:
     bool startIncoming(const QString& remoteOffer);
     bool applyRemoteAnswer(const QString& remoteAnswer);
     bool addRemoteCandidate(const QString& candidate, const QString& mid, int mLineIndex);
+    bool sendMediaPacket(const QByteArray& packet);
     void close();
 
 signals:
     void localOfferCreated(const QString& sdp);
     void localAnswerCreated(const QString& sdp);
     void localCandidateCreated(const QString& candidate, const QString& mid, int mLineIndex);
+    void mediaPacketReceived(const QByteArray& packet);
     void connected();
     void disconnected();
     void error(const QString& message);
 
 private:
     void setupPeerConnection();
+    void configureMediaChannel();
 
 #ifdef HAVE_LIBDATACHANNEL
     std::shared_ptr<rtc::PeerConnection> peer_connection_;
     std::shared_ptr<rtc::DataChannel> heartbeat_channel_;
+    std::shared_ptr<rtc::DataChannel> media_channel_;
 #endif
 };
 
